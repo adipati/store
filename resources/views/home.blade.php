c9b3246f34b4f49238f546742cfb3a1621804b14
adipati <aqu.monez63@gmail.com>
2018-05-10 18:55:31 +0700

    add counter transaction

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
 <section class="content">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-4">
             <div class="info-box">
                 <a href="{{ route('distributors.index') }}">
                     <span class="info-box-icon bg-aqua">
@@ -20,7 +20,7 @@
                 </a>
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
             <div class="info-box">
                 <a href="{{ route('products.index') }}">
                     <span class="info-box-icon bg-red">
@@ -29,6 +29,19 @@
                     <div class="info-box-content">
                         <span class="info-box-text">Jumlah Produk</span>
                         <span class="info-box-number">{{$products}}</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="info-box">
+                <a href="{{ route('transactions.index') }}">
+                    <span class="info-box-icon bg-orange">
+                        <i class="ion ion-cart"></i>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Jumlah Transaksi</span>
+                        <span class="info-box-number">{{$transactions}}</span>
                     </div>
                 </a>
             </div>
